176dbb7e431fc57e4825f1989f0a2a3d5dd1f32b
reSe1-2024 <emmanuelkamande39@gmail.com>
2024-05-09 17:24:24 +0300

    some explanations na fix moja

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let currentPlayer = X_PLAYER
 let spaces = Array(9).fill(null)//CREATES 9 EMPTY SPACES
 //ADD AN EVENT LISTENER TO EACH OF OUR ID'S
 
-const startGame = function(){
+  function startGame  (){
  boxes.forEach(function(box){
     box.addEventListener("click", boxClicked)})
 }
@@ -21,7 +21,7 @@ const startGame = function(){
 function boxClicked(e){
     const id = e.target.id
 
-    if(!spaces[id] ){
+    if(!spaces[id] ){ // falsy values
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
 
@@ -30,11 +30,16 @@ function boxClicked(e){
             playerText.textContent = `${currentPlayer} has WON!`
             let winning_blocks = playerHasWon();
      if (winning_blocks) {
-       for (var i = 0; i < winning_blocks.length; i++) {
-         var box = winning_blocks[i];
+       for (let i = 0; i < winning_blocks.length; i++) {
+         let box = winning_blocks[i];
        boxes[box].style.backgroundColor = winnerIndicator;
+      
   }
 }
+
+return;
+        }else{
+            playerText.textContent = `💭 Hmm!`
         }
         
         if (currentPlayer === 'X' ) {
@@ -81,7 +86,28 @@ function restart(){
         box.innerText = ''
         box.style.backgroundColor =''
             })
-            playerText.textContent = ""
+            playerText.textContent = "Tic Tac Toe"
             currentPlayer = X_PLAYER
 }
 startGame()
+
+/* 1.contains an array of elements which we gave the class box which rep the   9  squares.
+2.Has the restart button
+3.Has the playerText which contains the current player or winner.
+4.Has the winnerIndicator which basically stores the background color for the winning blocks.
+5.the players
+ 6. An array of 9 null values.
+ 7.startGamefunction adds an event listener to each of the 9 squares when you click on a box the boxclicked function is called.
+ 8.  boxClicked function
+-It gets the id of the clicked square and checks if the square is empty (i.e., spaces[id] is null).
+-If the square is empty, it sets the square's value to the current player (spaces[id] = currentPlayer) and updates the square's text content to the current player.
+-It then calls the playerHasWon function to check if the current player has won.
+-If the current player has won, it updates the playerText element to display the winner and highlights the winning blocks by setting their background color to the winnerIndicator value.
+-If the current player has not won, it switches the current player to the other player (i.e., "X" becomes "O" and vice versa).
+9.playerHasWon function
+
+-This function checks if the current player has won by iterating through the winningCombos array.
+-Each element in winningCombos is an array of three indices that represent a possible winning combination (e.g., [0, 1, 2] represents the top row).
+-For each combination, it checks if all three squares are occupied by the same player (i.e., spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c]).
+-If a winning combination is found, it returns the indices of the winning blocks. Otherwise, it returns false.
+ */
